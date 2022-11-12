ef889597b38476ef8ff168b478b27a24fd0f105b
mxtlrr <ruson0914@gmail.com>
2022-11-12 13:59:36 -0600

    remove alot of things

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,27 +3,18 @@
 #include <stdint.h>
 
 #include "libc/stdio.h"
+
+/* memory */
 #include "mem/mmap.h"
 #include "mem/mm.h"
 #include "multiboot.h"
 
 extern void load_gdt();
 
-void kmain(multiboot_info_t* mbd, uint32_t magic){
+void kmain(){
   printf("Hello world!\n\nMemory map:\n");
 
-  // Now we have the memory map!
-  block_t* b = getmmap(mbd, magic);
-  printmmap(mbd, magic);
-
-  // test malloc
-  uint32_t m = malloc(b, 4); // allocate 4 bytes
-  printf("\n\n4 bytes have been allocated to location: 0x%x", m);
-
-  // okay, now let's free this memory
-  free(m);
-
-  // yuh!
+  // Load gdt
   load_gdt();
   printf("GDT has been enabled!!");
 }
