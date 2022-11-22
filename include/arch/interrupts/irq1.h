e23d64ecdf94671926eda284c29187cea864089e
mxtlrr <ruson0914@gmail.com>
2022-11-22 10:47:00 -0600

    layout for irq1

---
+++ include/arch/interrupts/irq1.h
@@ -0,0 +1,13 @@
+/* IRQ1 ---> Keyboard */
+#pragma once
+
+#include "arch/idt.h" // on the safe side!
+#include "libc/stdio.h"
+#include "libc/stdio_debugcon.h"
+
+// Initialize the keyboard interrupt
+void init_kbd();
+
+
+// Function that gets called whenever there's
+// a keyboard int.
